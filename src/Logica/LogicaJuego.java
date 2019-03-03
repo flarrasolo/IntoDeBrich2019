3af85a604e307223d09d2ef3aa7c6920fcd3767f
flarrasolo <franciscolarrasolo@gmail.com>
2019-03-03 16:57:14 -0300

    Prueba Repo 1

--- src/Logica/LogicaJuego.java
+++ src/Logica/LogicaJuego.java
@@ -4,6 +4,8 @@ import Grafica.ComponenteGrafico;
 
 public class LogicaJuego {
 	private ComponenteGrafico [][] tablero;
+	
+	//Constructor
 	public LogicaJuego() {
 		tablero = new ComponenteGrafico[8][8];
 	}
