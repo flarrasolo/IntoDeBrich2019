462093e2f08d5d3f2f0ee0946edb9f1fd4003a89
flarrasolo <franciscolarrasolo@gmail.com>
2019-03-03 16:51:27 -0300

    Prueba primer Commit

---
+++ src/Logica/LogicaJuego.java
@@ -0,0 +1,10 @@
+package Logica;
+
+import Grafica.ComponenteGrafico;
+
+public class LogicaJuego {
+	private ComponenteGrafico [][] tablero;
+	public LogicaJuego() {
+		tablero = new ComponenteGrafico[8][8];
+	}
+}
